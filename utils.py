befb89bd337114e71d94362f6bdd7f48e5599a30
Douglas Silva <douglas_hss@hotmail.com.br>
2017-07-19 11:53:00 -0300

    Organizando projeto em arquivos especificos.

---
+++ utils.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import string
+
+from collections import Counter
+from nltk.corpus import stopwords
+from unicodedata import normalize
+
+
+STOPWORDS = set(stopwords.words('english'))
+PONTUACAO = set(string.punctuation)
+
+
+def normalizar_texto(texto):
+    """Função que substitui caracteres especiais por caracteres normais.
+        :param texto: :unicode:
+
+        :return :unicode:
+    """
+    return normalize('NFKD', texto).lower()
+
+
+def tokenizar(texto):
+    """Função de tokenização de um texto. Stopwords e Pontuações serão removidas.
+        :param texto: :unicode:
+
+        :return :dict: key:token value:frequencia
+    """
+    counter = Counter(normalizar_texto(texto).split())
+    return {token: frequencia
+            for token, frequencia in counter.items()
+            if token not in STOPWORDS or token not in PONTUACAO}
